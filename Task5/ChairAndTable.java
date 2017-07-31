5545ae98029d642d85bd1a080e58d554e5ecda0c
jc259968 <janrich.vanheerden@my.jcu.edu.au>
2017-07-31 14:36:33 +1000

    Finish CODING exercises

---
+++ Task5/ChairAndTable.java
@@ -0,0 +1,15 @@
+package Task5;
+
+/**
+ * Created by jc259968 on 31/07/17.
+ */
+public class ChairAndTable {
+    public static void main(String[] args) {
+        System.out.println("X                       X");
+        System.out.println("X                       X");
+        System.out.println("X                       X");
+        System.out.println("XXXX    XXXXXXXX    XXXXX");
+        System.out.println("X  X    X      X    X   X");
+        System.out.println("X  X    X      X    X   X");
+    }
+}
---
+++ Task5/MovieQuotes.java
@@ -0,0 +1,13 @@
+package Task5;
+
+/**
+ * Created by jc259968 on 31/07/17.
+ */
+public class MovieQuotes {
+    public static void main(String[] args) {
+        System.out.println("YOU SHALL NOT PASS!!!!!");
+        System.out.println("Gandalf");
+        System.out.println("Lord of the Rings: The Fellowship of the Ring");
+        System.out.println("2001");
+    }
+}
